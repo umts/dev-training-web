f8ffc7a25fe2b0caee25485eb1913cdd1fdbb2fb
Matt Moretti <werebus@gmail.com>
2022-03-10 16:34:01 -0500

    Favicon

--- lib/dev_training_application.rb
+++ lib/dev_training_application.rb
@@ -1,6 +1,7 @@
 class DevTrainingApplication < Sinatra::Base
   configure do
     set :root, File.join(File.dirname(settings.app_file), '..')
+    set :static, settings.production? ? false : true
     set :haml, layout: :application
   end
 
