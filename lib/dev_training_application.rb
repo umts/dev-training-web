77f0dbb217db8fc1b57a84053fc2f541773eedea
Matt Moretti <werebus@gmail.com>
2022-03-10 15:36:16 -0500

    Added Haml and layout

--- lib/dev_training_application.rb
+++ lib/dev_training_application.rb
@@ -1,5 +1,10 @@
 class DevTrainingApplication < Sinatra::Base
+  configure do
+    set :root, File.join(File.dirname(settings.app_file), '..')
+    set :haml, layout: :application
+  end
+
   get '/' do
-    'Hello'
+    haml :index
   end
 end
